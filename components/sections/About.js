9c875e97d0cddcb5607b247afbd8b2a33b90e62a
Alexandre <120102637+Alexandrefranck@users.noreply.github.com>
2023-04-01 15:54:05 +0200

    first commit

---
+++ components/sections/About.js
@@ -0,0 +1,114 @@
+import styles from "../../styles/About.module.scss";
+import Titles from "../Titles";
+import Image from "next/image";
+import Avatar from "@/public/photos/avatar.jpg";
+function About() {
+  const aboutAchievements = [
+    {
+      icon: "bi-hand-thumbs-up",
+      number: 198,
+      name: "Projects completed",
+    },
+    {
+      icon: "bi-cup-hot",
+      number: 5670,
+      name: "Cup of coffee",
+    },
+    {
+      icon: "bi-people",
+      number: 427,
+      name: "Satisfied clients",
+    },
+    {
+      icon: "bi-award",
+      number: 35,
+      name: "Nominees winner",
+    },
+  ];
+  const aboutSkills = [
+    {
+      name: "HTML5 / CSS3 / Bootstrap",
+      lvl: 70,
+      bgColor: "rgb(108, 108, 229)",
+    },
+    {
+      name: "Javascript",
+      lvl: 75,
+      bgColor: "rgb(249, 215, 76)",
+    },
+    {
+      name: "React / Next.js",
+      lvl: 60,
+      bgColor: "#FF4C60",
+    },
+  ];
+
+  return (
+    <div id="about" className="container mt-section">
+      <Titles value="A propos"></Titles>
+      <div className="row">
+        <div className="col-lg-2 d-none d-lg-block">
+          <Image
+            src={Avatar}
+            width={186}
+            height={206}
+            className="rounded-circle"
+          />
+          {/* <img src="https://jthemes.net/themes/wp/bolby/bolby5/wp-content/uploads/sites/5/2021/01/avatar-1-2.svg" className='w-100' alt="" /> */}
+        </div>
+        <div className="col-lg-10 ps-md-5 p-4">
+          <div
+            className={`${styles.dialog} row bg-white gx-md-5 rounded-4 shadow p-4 position-relative`}
+          >
+            <div className="col-md-6">
+              <p className="lh-lg">
+                J'ai actuellement 23 ans, en reconversion professionnelle après
+                avoir travaillé 5 ans en restauration, passionner aussi de rugby
+                ce qui m'a permit d'apprendre ce que c'est le travail collectif,
+                savoir m'adapter et rebondir est l'un de mes plus gros atouts !
+                Souriant, joyeux à longueur de journée, je serai prêt à relever,
+                n'importe quel défi !
+              </p>
+              <a
+                target="_blank"
+                href="/CV_Alexandre.pdf"
+                className="btn btn btn-primary mt-2"
+              >
+                Download CV
+              </a>
+            </div>
+            <div className="col-md-6">
+              <div className="d-flex flex-column justify-content-between h-100">
+                {aboutSkills.map((skill, i) => (
+                  <div key={i}>
+                    <div className="d-flex align-items-center justify-content-between mt-4 mt-md-0">
+                      <strong>{skill.name}</strong>
+                      <span>{skill.lvl}%</span>
+                    </div>
+                    <div
+                      className={`${styles.progress} progress mt-3`}
+                      role="progressbar"
+                      aria-valuenow={skill.lvl}
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    >
+                      <div
+                        style={{
+                          width: skill.lvl + "%",
+                          backgroundColor: skill.bgColor,
+                        }}
+                        className={styles.progressBar}
+                      ></div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ components/sections/Experience.js
@@ -0,0 +1,97 @@
+import styles from '../../styles/Experience.module.scss';
+import Titles from '../Titles'
+import Image from 'next/image';
+
+function Experience() {
+
+    const experiences = [
+        {
+            yearStart: 2019,
+            yearEnd: null,
+            title: "Mediaschool Nice",
+            organization: "Bachelor SupdewebDev",
+            description: "Actuellement étudiant pour devenir développeur Full-stack après mon Bachelor.",
+            type: "study" // study / job
+        },
+        {
+            yearStart: 2014,
+            yearEnd: 2017,
+            title: "Hôtellerie restauration",
+            organization: "option cuisine",
+            description: "Diplômé en restauration en 2017 à Draguignan dans la spécialisation cuisinier.",
+            type: "study" // study / job
+        },
+        
+        {
+            yearStart: 2021,
+            yearEnd: 2022,
+            title: "Chef de cuisine",
+            organization: "Restaurant au 13 à Draguignan",
+            description: "Planification et création de menus : Capacité à planifier et à créer des menus innovants en utilisant des ingrédients de saison, en s'assurant que les plats sont bien équilibrés et présentés de manière attrayante.",
+            type: "job" // study / job
+        },
+        {
+            yearStart: "Été 2021",
+            yearEnd: "Été 2022",
+            title: "Chef de rang ,serveur",
+            organization: "Le bistrot à Draguignan",
+            description: "Gestion d'équipe : Expérience de la gestion d'une équipe de serveurs, la formation et le développement de l'équipe pour atteindre les objectifs de l'entreprise.",
+            type: "job" // study / job
+        },
+        {
+            yearStart: 2020,
+            yearEnd: 2021,
+            title: "Préparateur de commande",
+            organization: "ID Logistique au Arcs sur Argens",
+            description: "Connaissance des processus de préparation de commande : Expérience de la préparation de commande dans un environnement d'entrepôt, y compris la connaissance des techniques de cueillette, de tri et de conditionnement des produits pour garantir une expédition ou un stockage efficace",
+            type: "job" // study / job
+        },
+        
+    ]
+
+    return (
+        <div id="experiences" className='container mt-section'>
+            <Titles value="Expériences" />
+            <div className="row mt-5">
+                <div className="col-md-6 ">
+                    <div className='rounded-4 ps-4 pe-5 py-4 bg-white shadow'>
+                        <div className='border-start border-danger'>
+                            <div className='d-flex flex-column gap-5'>
+                                {experiences.slice(0, experiences.length / 2).map((experience, i) =>
+                                    <Competence key={i} experience={experience} />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className='rounded-4 ps-4 pe-5 py-4 bg-white shadow mt-4 mt-md-0'>
+                    <div className='border-start border-danger'>
+                        <div className='d-flex flex-column gap-5'>
+                            {experiences.slice(experiences.length / 2, experiences.length).map((experience, i) =>
+                                <Competence key={i} experience={experience} />
+                            )}
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Competence({ experience }) {
+    return (
+        <div className={`ps-5 position-relative ${styles.competence}`}>
+            {experience.type === "study" ? <span className={styles.studyIcon}>
+                <i className={`${styles.icon} text-danger bi bi-mortarboard`}></i>
+            </span> : ""}
+
+            <small className='text-muted d-block'>{experience.yearStart} - {experience.yearEnd === null ? "Present" : experience.yearEnd}</small>
+            <h3 className='mt-2 fs-5 d-inline-block'>{experience.title}</h3> - <small>{experience.organization}</small>
+            <p className='m-0'>{experience.description}</p>
+        </div>
+    )
+}
+
+export default Experience;
